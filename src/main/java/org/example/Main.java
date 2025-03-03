90860b92f6a11364c7fb6d9a52fe0089af029d4a
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-03-03 15:30:31 +0300

    Wrote all methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,37 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 24) return false;
+        return isBarking && (clock < 8 || clock > 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        int[] ages = {firstAge, secondAge, thirdAge};
+
+        for (int age: ages) {
+            if (age >= 13 && age <= 19) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     if (isSummer && temp >= 25 && temp <= 45) {
+        return true;
+     } else if (!isSummer && temp >= 25 && temp <= 35) {
+         return true;
+     }
+     return false;
     }
 
     public static double area(double width, double height) {
-       
+       if (width < 0 || height < 0) return -1;
+       return width * height;
     }
 
     public static double area(double radius) {
-       
+       if (radius < 0) return -1;
+       return radius * radius * Math.PI;
     }
 }
